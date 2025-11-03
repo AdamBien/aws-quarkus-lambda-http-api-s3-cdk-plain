5c2f6fe3fbe614554398ab7e04195fe35d9a44b1
Adam Bien <abien@adam-bien.com>
2025-11-03 15:07:45 +0100

    superfluous config properties removed

--- cdk/src/main/java/airhacks/Configuration.java
+++ cdk/src/main/java/airhacks/Configuration.java
@@ -8,15 +8,6 @@ import software.amazon.awscdk.StackProps;
 
 public record Configuration(String appName) {
 
-    public final static String addressTableNameKey = "address.table.name";
-    public final static String addressTableNameKeyAsEnvEntry = addressTableNameKey
-            .replace(".", "_")
-            .toUpperCase();
-
-    public final static String addressBucketNameKey = "address.bucket.name";
-    public final static String addressBucketNameKeyAsEnvEntry = addressBucketNameKey
-            .replace(".", "_")
-            .toUpperCase();
 
     public StackProps stackProperties() {
         ZCfg.load(appName);
@@ -36,15 +27,6 @@ public record Configuration(String appName) {
         return ZCfg.string("function.name");
     }
 
-    public String addressTableName() {
-        ZCfg.load(appName);
-        return ZCfg.string(addressTableNameKey, "Addresses");
-    }
-
-    public String addressBucketName() {
-        ZCfg.load(appName);
-        return ZCfg.string(addressBucketNameKey, "addresses-bucket");
-    }
 
     public List<String> allowOrigins() {
         ZCfg.load(appName);
