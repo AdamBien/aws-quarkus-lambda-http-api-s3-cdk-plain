ac6b63f4a74ddbc2af2cb2452d83e7103bf8047b
Adam Bien <abien@adam-bien.com>
2025-11-03 15:06:50 +0100

    Random bucket name generated

--- cdk/src/main/java/airhacks/s3/boundary/BucketStack.java
+++ cdk/src/main/java/airhacks/s3/boundary/BucketStack.java
@@ -4,7 +4,7 @@ import airhacks.Configuration;
 import airhacks.ConventionalDefaults;
 import airhacks.s3.control.Buckets;
 import software.amazon.awscdk.Stack;
-import software.amazon.awscdk.Tags;
+import software.amazon.awscdk.services.s3.Bucket;
 import software.constructs.Construct;
 
 /**
@@ -12,11 +12,15 @@ import software.constructs.Construct;
  */
 public class BucketStack extends Stack {
 
+    Bucket addressesBucket;
+
     public BucketStack(Construct scope, Configuration configuration) {
         super(scope, ConventionalDefaults.stackName(configuration.appName(), "bucket"),
                 configuration.stackProperties());
+        this.addressesBucket = Buckets.createAddressesBucket(this, configuration);
+    }
 
-        var bucket = Buckets.createAddressesBucket(this, configuration);
-        Tags.of(bucket).add("BucketName", configuration.addressBucketName());
+    public String bucketName(){
+        return this.addressesBucket.getBucketName();
     }
 }
